bc9746b2175da8cd4ede5a3ec2bcd2521c6f3bfe
Chase Lirette <chaselirette@yahoo.com>
2017-03-29 17:40:01 -0500

    Added routes and templates

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/members', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/events', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/acmw', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/loginpage', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/calendar', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/spgrp', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 module.exports = router;
