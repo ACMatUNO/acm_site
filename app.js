224afaff72c3e2783ffc560e79089becc1a11f87
Chase Lirette <chaselirette@yahoo.com>
2017-04-05 19:02:12 -0500

    Created favicon and added it to site.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view options', {layout: 'layout'});
 hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
